9653397a3751fe3c8d03933e1e7b003357c86a9d
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-10-06 10:14:52 -0400

    asdasdsa

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+
+// function getBMI(){
+//     const weight=Number(prompt("enter your weight in lbs"));
+//     const height=Number(prompt("enter height in inches"));
+//     const BMI = (703 * weight/(height**2));
+//     return BMI;
+// }
+
+// function checkBMI(BMI){
+//         if (BMI <= 25){ 
+//         console.log("healthy");
+//     } 
+//     else if (BMI => 15){
+//         console.log("unhealthy");
+//     } 
+//     else {
+//         console.log("unhealthy");
+//     }
+//     console.log(BMI)
+// }
+
+
+// function init(){
+//     const BMI = getBMI();
+//     checkBMI(BMI);
+// }
+
+// init()
+
+
+// // function check(y){
+// //     if (y>10) {
+// //         console.log("big too")
+// //     }
+// //     else if (y<=1){
+// //         console.log("too smol")
+// //     }
+// //     else{
+// //         console.log("noice")
+// //     }
+// // }
+
+// // check(2)
